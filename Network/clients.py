4707f041961737c2551e2a65e2d93d13d35e3c76
Laszlo-prog <68603866+Laszlo-prog@users.noreply.github.com>
2025-10-31 21:20:25 +0200

    Add client socket implementation in clients.py
    Decode message from server side

---
+++ Network/clients.py
@@ -0,0 +1,10 @@
+import socket
+host = 'localhost'
+port = 8080
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+message = s.recv(1024)
+while message:
+    print("Message":message.decode('utf-8'))
+    message = s.recv(1024)
+s.close()
